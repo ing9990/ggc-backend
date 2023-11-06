c91df0be1343f7ab81e39de12f6ed18169130b5a
ing9990 <mail80782258@gmail.com>
2023-11-06 11:30:47 +0900

    Initial commit

---
+++ src/main/java/com/gigacoffeebackend/GigacoffeeBackendApplication.java
@@ -0,0 +1,13 @@
+package com.gigacoffeebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GigacoffeeBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GigacoffeeBackendApplication.class, args);
+    }
+
+}
