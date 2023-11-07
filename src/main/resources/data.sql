2606ae61b53a301d76d234f89ae9668d66f5f20f
ing9990 <mail80782258@gmail.com>
2023-11-08 08:53:13 +0900

    기타 설정파일 추가

---
+++ src/main/resources/data.sql
@@ -0,0 +1,2 @@
+INSERT INTO users(ID, CREATED_DATE_TIME, MODIFIED_DATE_TIME, DISPLAY_NAME, IMAGE_URL, NICK_NAME, SOCIAL_LOGIN_ID)
+values (1, now(), now(), '김기가#6789', 'https://image.png', '김기가', '123456789');
