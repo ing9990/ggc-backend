174d4de454034681f3b9f0734a7e4bc504144db4
ing9990 <mail80782258@gmail.com>
2023-11-09 17:55:43 +0900

    update submodule

--- src/main/java/com/gigacoffeebackend/auth/ui/AuthApi.java
+++ src/main/java/com/gigacoffeebackend/auth/ui/AuthApi.java
@@ -1,17 +1,16 @@
 package com.gigacoffeebackend.auth.ui;
 
-import com.gigacoffeebackend.auth.domain.AccessAndRefreshToken;
-import com.gigacoffeebackend.auth.dto.AccessTokenResponse;
+import com.gigacoffeebackend.auth.domain.AuthService;
 import com.gigacoffeebackend.global.dto.ApiResponse;
+import com.gigacoffeebackend.login.ui.AccessTokenResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletResponse;
 
-import static org.springframework.http.HttpHeaders.SET_COOKIE;
+import static org.springframework.http.HttpHeaders.AUTHORIZATION;
 import static org.springframework.http.HttpStatus.CREATED;
 
 @RequestMapping("/api/v1/auth")
@@ -21,28 +20,16 @@ import static org.springframework.http.HttpStatus.CREATED;
 public class AuthApi {
 
     public static final int COOKIE_AGE_SECONDS = 604800;
-    private final AuthLogin authLogin;
+    private final AuthService authService;
 
-    @GetMapping("/login/{provider}")
-    public ResponseEntity<ApiResponse<AccessTokenResponse>> login(
-            @PathVariable final String provider,
-            @RequestParam(value = "code") String code,
-            final HttpServletResponse response
+    @PostMapping("/token")
+    public ResponseEntity<ApiResponse<AccessTokenResponse>> extendToken(
+            @CookieValue(name = "refresh-token") final String refreshToken,
+            @RequestHeader(AUTHORIZATION) final String accessToken,
+            HttpServletResponse response
     ) {
-        final AccessAndRefreshToken accessAndRefreshToken = authLogin.connect(provider, code);
+        String accessAndRefreshToken = authService.renewalAccessToken(refreshToken, accessToken);
 
-        final ResponseCookie cookie = ResponseCookie.from("refresh-token", accessAndRefreshToken.getRefreshToken())
-                .maxAge(COOKIE_AGE_SECONDS)
-                .sameSite("None")
-                .secure(true)
-                .httpOnly(false)
-                .path("/")
-                .build();
-        response.addHeader(SET_COOKIE, cookie.toString());
-
-        return ResponseEntity.status(CREATED).body(ApiResponse.ok(new AccessTokenResponse(accessAndRefreshToken.getAccessToken())));
+        return ResponseEntity.status(CREATED).body(ApiResponse.ok(new AccessTokenResponse(accessAndRefreshToken)));
     }
-
-
-
 }
