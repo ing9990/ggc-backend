234743ef45ff6a2430b637c7f4e190fdcb96876d
ing9990 <mail80782258@gmail.com>
2023-11-13 11:33:07 +0900

    테스트 SQL 스크립트 User 도메인에 맞도록 수정

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,3 @@
-INSERT INTO users(ID, CREATED_DATE_TIME, MODIFIED_DATE_TIME, DISPLAY_NAME, IMAGE_URL, NICK_NAME, SOCIAL_LOGIN_ID)
-values (1, now(), now(), '김기가#6789', 'https://image.png', '김기가', '123456789');
+INSERT INTO users(ID, CREATED_DATE_TIME, MODIFIED_DATE_TIME, DISPLAY_NAME, IMAGE_URL, NICK_NAME, SOCIAL_LOGIN_ID,
+                  AUTHORITY)
+values (1, now(), now(), '김기가#6789', 'https://image.png', '김기가', '123456789', 'USER');
